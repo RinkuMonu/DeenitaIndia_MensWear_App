ef35f9290e51e662f1bcaa685076031e9cf6574c
vinod <vinod@7unique.in>
2026-02-07 12:45:28 +0530

    Notification Completed

--- lib/view/wishlistScreen.dart
+++ lib/view/wishlistScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../constants/image.dart';
+import '../widgets/customAppBar.dart';
 
 class WishlistScreen extends StatefulWidget {
   const WishlistScreen({super.key});
@@ -11,7 +15,48 @@ class _WishlistScreenState extends State<WishlistScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: const CustomAppBar(
+        title: "WishList",
+        showMenu: false,
+        appBarBgColor: Colors.white,
+        showWishlist: false,
+      ),
+      body: Center(
+        child: _emptyNotification(),
+      )
+
 
     );
   }
+
+
+  Widget _emptyNotification() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // SvgPicture.asset(AppImage.emptyWishList, height: 55),
+        const SizedBox(height: 12),
+        const Text(
+          "Wishlist is empty!",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 20,
+          ),
+        ),
+        const SizedBox(height: 8),
+        const Text(
+          "Add items you love to your \nwishlist",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontWeight: FontWeight.w300,
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    );
+  }
+
 }
